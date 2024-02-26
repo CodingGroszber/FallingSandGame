1f8d8dd30a1acbb66bfbfb22f749ed835ea14290
CodingGroszber <160913111+CodingGroszber@users.noreply.github.com>
2024-02-26 15:42:12 +0100

    Initial repo setup with all files and stuff.

---
+++ FallingSandGame/RayLibSetup/DevopsDatalab.cs
@@ -0,0 +1,127 @@
+﻿//using System;
+//using Raylib_cs;
+
+//namespace FallingSandGameDevopsDatalab
+//{
+//	class Program
+//	{
+//		// Constants for the game window size
+//		const int screenWidth = 800;
+//		const int screenHeight = 600;
+
+//		// Constants for the sand particle properties
+//		const int particleSize = 4;
+//		const int particleSpeed = 2;
+
+//		// 2D array to represent the game world
+//		static int[,] gameWorld;
+
+//		static void Main(string[] args)
+//		{
+//			// Initialize the game window
+//			Raylib.InitWindow(screenWidth, screenHeight, "Falling Sand Game");
+
+//			// Initialize the game world
+//			gameWorld = new int[screenWidth / particleSize, screenHeight / particleSize];
+
+//			// Set up the game world with initial sand particles
+//			InitializeGameWorld();
+
+//			// Game loop
+//			while (!Raylib.WindowShouldClose())
+//			{
+//				// Update
+//				UpdateGame();
+
+//				// Draw
+//				DrawGame();
+//			}
+
+//			// Close the game window
+//			Raylib.CloseWindow();
+//		}
+
+//		static void InitializeGameWorld()
+//		{
+//			// Set up the game world with initial sand particles
+//			for (int x = 0; x < gameWorld.GetLength(0); x++)
+//			{
+//				for (int y = 0; y < gameWorld.GetLength(1); y++)
+//				{
+//					if (y == 0)
+//					{
+//						// Place sand particles at the top of the game world
+//						gameWorld[x, y] = 1;
+//					}
+//					else
+//					{
+//						// Empty space
+//						gameWorld[x, y] = 0;
+//					}
+//				}
+//			}
+//		}
+
+//		static void UpdateGame()
+//		{
+//			// Update the game world
+//			for (int x = 0; x < gameWorld.GetLength(0); x++)
+//			{
+//				for (int y = gameWorld.GetLength(1) - 1; y >= 0; y--)
+//				{
+//					if (gameWorld[x, y] == 1)
+//					{
+//						// Check if there is empty space below the sand particle
+//						if (y < gameWorld.GetLength(1) - 1 && gameWorld[x, y + 1] == 0)
+//						{
+//							// Move the sand particle down
+//							gameWorld[x, y] = 0;
+//							gameWorld[x, y + 1] = 1;
+//						}
+//						else
+//						{
+//							// Check if there is empty space diagonally below the sand particle
+//							if (y < gameWorld.GetLength(1) - 1 && x > 0 && gameWorld[x - 1, y + 1] == 0)
+//							{
+//								// Move the sand particle diagonally down-left
+//								gameWorld[x, y] = 0;
+//								gameWorld[x - 1, y + 1] = 1;
+//							}
+//							else if (y < gameWorld.GetLength(1) - 1 && x < gameWorld.GetLength(0) - 1 && gameWorld[x + 1, y + 1] == 0)
+//							{
+//								// Move the sand particle diagonally down-right
+//								gameWorld[x, y] = 0;
+//								gameWorld[x + 1, y + 1] = 1;
+//							}
+//						}
+//					}
+//				}
+//			}
+//		}
+
+//		static void DrawGame()
+//		{
+//			// Begin drawing
+//			Raylib.BeginDrawing();
+
+//			// Clear the screen
+//			Raylib.ClearBackground(Color.White);
+
+//			// Draw the game world
+//			for (int x = 0; x < gameWorld.GetLength(0); x++)
+//			{
+//				for (int y = 0; y < gameWorld.GetLength(1); y++)
+//				{
+//					if (gameWorld[x, y] == 1)
+//					{
+//						// Draw sand particles
+//						Raylib.DrawRectangle(x * particleSize, y * particleSize, particleSize, particleSize, Color.Yellow);
+//					}
+//				}
+//			}
+
+//			// End drawing
+//			Raylib.EndDrawing();
+//		}
+//	}
+//}
---
+++ FallingSandGame/RayLibSetup/Program.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using Raylib_cs;
+
+namespace FallingSandGame
+{
+	class Program
+	{
+		static void Main()
+		{
+			const int screenWidth = 800;
+			const int screenHeight = 600;
+			const int cellSize = 5; // Size of each cell (pixel)
+
+			Raylib.InitWindow(screenWidth, screenHeight, "Falling Sand Game");
+
+			// Create a 2D grid to store material types
+			int gridWidth = screenWidth / cellSize;
+			int gridHeight = screenHeight / cellSize;
+			char[,] grid = new char[gridWidth, gridHeight];
+
+			// Initialize grid with empty cells
+			for (int x = 0; x < gridWidth; x++)
+			{
+				for (int y = 0; y < gridHeight; y++)
+				{
+					grid[x, y] = ' ';
+				}
+			}
+
+			// Main game loop
+			while (!Raylib.WindowShouldClose())
+			{
+				// User input
+				if (Raylib.IsMouseButtonDown(MouseButton.Left))
+				{
+					// Add material (e.g., sand) where the mouse is clicked
+					int mouseX = Raylib.GetMouseX() / cellSize;
+					int mouseY = Raylib.GetMouseY() / cellSize;
+					if (mouseX >= 0 && mouseX < gridWidth && mouseY >= 0 && mouseY < gridHeight)
+					{
+						grid[mouseX, mouseY] = 'S'; // 'S' for sand
+					}
+				}
+				else if (Raylib.IsMouseButtonDown(MouseButton.Right))
+				{
+					// Remove material where the mouse is clicked
+					int mouseX = Raylib.GetMouseX() / cellSize;
+					int mouseY = Raylib.GetMouseY() / cellSize;
+					if (mouseX >= 0 && mouseX < gridWidth && mouseY >= 0 && mouseY < gridHeight)
+					{
+						grid[mouseX, mouseY] = ' '; // Empty cell
+					}
+				}
+
+				// Update physics (e.g., gravity and rolling)
+				for (int x = 0; x < gridWidth; x++)
+				{
+					for (int y = gridHeight - 1; y >= 0; y--)
+					{
+						if (grid[x, y] == 'S')
+						{
+							// Sand falls down
+							if (y < gridHeight - 1 && grid[x, y + 1] == ' ')
+							{
+								grid[x, y] = ' ';
+								grid[x, y + 1] = 'S';
+							}
+							else
+							{
+								// Sand rolls left or right randomly
+								int rollDirection = new Random().Next(2); // 0 or 1
+								int newX = x + (rollDirection == 0 ? -1 : 1);
+								if (newX >= 0 && newX < gridWidth && grid[newX, y] == ' ')
+								{
+									grid[x, y] = ' ';
+									grid[newX, y] = 'S';
+								}
+							}
+						}
+					}
+				}
+
+				// Draw grid
+				Raylib.BeginDrawing();
+				Raylib.ClearBackground(Color.White);
+				for (int x = 0; x < gridWidth; x++)
+				{
+					for (int y = 0; y < gridHeight; y++)
+					{
+						if (grid[x, y] == 'S')
+						{
+							Raylib.DrawRectangle(x * cellSize, y * cellSize, cellSize, cellSize, Color.Yellow);
+						}
+					}
+				}
+				Raylib.EndDrawing();
+			}
+
+			Raylib.CloseWindow();
+		}
+	}
+}
+
+
+
+//using System;
+//using Raylib_cs;
+
+//namespace FallingSandGame
+//{
+//	class Program
+//	{
+//		static int screenWidth = 800;
+//		static int screenHeight = 600;
+//		static Color sandColor = Color.Yellow;
+//		static int sandSize = 3; // Size of sand particles
+//		static int sandSpeed = 2; // Speed at which sand falls
+
+//		static int[,] grid; // 2D grid to represent pixels/materials
+
+//		static void Main()
+//		{
+//			Raylib.InitWindow(screenWidth, screenHeight, "Falling Sand Game");
+
+//			// Initialize grid (0 = empty, 1 = sand)
+//			grid = new int[screenWidth / sandSize, screenHeight / sandSize];
+
+//			while (!Raylib.WindowShouldClose())
+//			{
+//				// Update
+//				UpdateSand();
+//				HandleUserInput();
+
+//				// Draw
+//				Raylib.BeginDrawing();
+//				Raylib.ClearBackground(Color.Black);
+//				DrawSand();
+//				Raylib.EndDrawing();
+//			}
+
+//			Raylib.CloseWindow();
+//		}
+
+//		static void UpdateSand()
+//		{
+//			// Simulate sand falling
+//			for (int x = 0; x < grid.GetLength(0); x++)
+//			{
+//				for (int y = 0; y < grid.GetLength(1); y++)
+//				{
+//					if (grid[x, y] == 1)
+//					{
+//						// Check if there's an empty space below
+//						if (y < grid.GetLength(1) - 1 && grid[x, y + 1] == 0)
+//						{
+//							// Move sand down
+//							grid[x, y] = 0;
+//							grid[x, y + 1] = 1;
+//						}
+//						else if (x > 0 && x < grid.GetLength(0) - 1)
+//						{
+//							// Randomly move left or right
+//							if (Raylib.GetRandomValue(0, 1) == 0)
+//							{
+//								if (grid[x - 1, y] == 0)
+//								{
+//									grid[x, y] = 0;
+//									grid[x - 1, y] = 1;
+//								}
+//							}
+//							else
+//							{
+//								if (grid[x + 1, y] == 0)
+//								{
+//									grid[x, y] = 0;
+//									grid[x + 1, y] = 1;
+//								}
+//							}
+//						}
+//					}
+//				}
+//			}
+//		}
+
+//		static void HandleUserInput()
+//		{
+//			// Add sand particles on left mouse click
+//			if (Raylib.IsMouseButtonDown(MouseButton.Left))
+//			{
+//				int mouseX = Raylib.GetMouseX() / sandSize;
+//				int mouseY = Raylib.GetMouseY() / sandSize;
+
+//				if (grid[mouseX, mouseY] == 0)
+//					grid[mouseX, mouseY] = 1;
+//			}
+
+//			// Erase sand particles while holding left mouse button
+//			//if (Raylib.IsMouseButtonDown(MouseButton.Right))
+//			{
+//				int mouseX = Raylib.GetMouseX() / sandSize;
+//				int mouseY = Raylib.GetMouseY() / sandSize;
+
+//				if (grid[mouseX, mouseY] == 1)
+//					grid[mouseX, mouseY] = 0;
+//			}
+//		}
+
+//		static void DrawSand()
+//		{
+//			for (int x = 0; x < grid.GetLength(0); x++)
+//			{
+//				for (int y = 0; y < grid.GetLength(1); y++)
+//				{
+//					if (grid[x, y] == 1)
+//					{
+//						// Draw sand particle
+//						Raylib.DrawRectangle(x * sandSize, y * sandSize, sandSize, sandSize, sandColor);
+//					}
+//				}
+//			}
+//		}
+//	}
+//}
